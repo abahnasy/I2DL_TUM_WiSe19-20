3c1ea7b6fcc53cc50d9ea36e10ad484793dcb6bc
asbahnasy <asbahnasy@icloud.com>
2019-11-29 23:30:12 +0100

    Initial repo commit + Solutions to Exercise 1

---
+++ exercise_1/exercise_code/classifiers/neural_net.py
@@ -0,0 +1,365 @@
+"""Two Layer Network."""
+# pylint: disable=invalid-name
+import numpy as np
+
+
+class TwoLayerNet(object):
+    """
+    A two-layer fully-connected neural network. The net has an input dimension
+    of N, a hidden layer dimension of H, and performs classification over C
+    classes. We train the network with a softmax loss function and L2
+    regularization on the weight matrices. The network uses a ReLU nonlinearity
+    after the first fully connected layer.
+
+    In other words, the network has the following architecture:
+
+    input - fully connected layer - ReLU - fully connected layer - softmax
+
+    The outputs of the second fully-connected layer are the scores for each
+    class.
+    """
+
+    def __init__(self, input_size, hidden_size, output_size, std=1e-4):
+        """
+        Initialize the model. Weights are initialized to small random values and
+        biases are initialized to zero. Weights and biases are stored in the
+        variable self.params, which is a dictionary with the following keys:
+
+        W1: First layer weights; has shape (D, H)
+        b1: First layer biases; has shape (H,)
+        W2: Second layer weights; has shape (H, C)
+        b2: Second layer biases; has shape (C,)
+
+        Inputs:
+        - input_size: The dimension D of the input data.
+        - hidden_size: The number of neurons H in the hidden layer.
+        - output_size: The number of classes C.
+        """
+        self.params = {}
+        self.params['W1'] = std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def loss(self, X, y=None, reg=0.0):
+        """
+        Compute the loss and gradients for a two layer fully connected neural
+        network.
+
+        Inputs:
+        - X: Input data of shape (N, D). Each X[i] is a training sample.
+        - y: Vector of training labels. y[i] is the label for X[i], and each
+          y[i] is an integer in the range 0 <= y[i] < C. This parameter is
+          optional; if it is not passed then we only return scores, and if it is
+          passed then we instead return the loss and gradients.
+        - reg: Regularization strength.
+
+        Returns:
+        If y is None, return a matrix scores of shape (N, C) where scores[i, c]
+        is the score for class c on input X[i].
+
+        If y is not None, instead return a tuple of:
+        - loss: Loss (data loss and regularization loss) for this batch of
+          training samples.
+        - grads: Dictionary mapping parameter names to gradients of those
+          parameters  with respect to the loss function; has the same keys as
+          self.params.
+        """
+        # pylint: disable=too-many-locals
+        # Unpack variables from the params dictionary
+        W1, b1 = self.params['W1'], self.params['b1']
+        W2, b2 = self.params['W2'], self.params['b2']
+        N, _ = X.shape
+
+        # Compute the forward pass
+        scores = None
+        ########################################################################
+        # TODO: Perform the forward pass, computing the class scores for the   #
+        # input. Store the result in the scores variable, which should be an   #
+        # array of shape (N, C).                                               #         
+        ########################################################################
+
+        relu = lambda x: np.maximum(0, x)
+        h = relu(X.dot(W1) + b1)
+        scores = h.dot(W2) + b2
+
+
+        ########################################################################
+        #                              END OF YOUR CODE                        #
+        ########################################################################
+
+        # If the targets are not given then jump out, we're done
+        if y is None:
+            return scores
+
+        # Compute the loss
+        loss = None
+        ########################################################################
+        # TODO: Finish the forward pass, and compute the loss. This should     #
+        # include both the data loss and L2 regularization for W1 and W2. Store#
+        # the result in the variable loss, which should be a scalar. Use the   #
+        # Softmax classifier loss. So that your results match ours, multiply   #
+        # the regularization loss by 0.5                                       #
+        ########################################################################
+        lim_scores = scores - np.max(scores) # Limit scores
+        # Array of correct scores
+        correct_scores = lim_scores[np.arange(N), y]
+        # Compute softmax loss
+        loss_array = - correct_scores + np.log(np.sum(np.exp(lim_scores), axis=1))    
+        loss = np.sum(loss_array)
+
+        # Normalize
+        loss /= N
+        # Regularize
+        loss += 0.5 * reg * (np.sum(W1 * W1) + np.sum(W2 * W2))
+        ########################################################################
+        #                              END OF YOUR CODE                        #
+        ########################################################################
+
+        # Backward pass: compute gradients
+        grads = {}
+        ########################################################################
+        # TODO: Compute the backward pass, computing the derivatives of the    #
+        # weights and biases. Store the results in the grads dictionary. For   #
+        # example, grads['W1'] should store the gradient on W1, and be a matrix#
+        # of same size                                                         #
+        ########################################################################
+
+        # W1
+        # ----+                      W2         b2
+        # dW2  \                     ----+      ----+
+        #      (*)----+              dW2  \     db2  \
+        # X    /       \                   \          \  
+        # ----+         \               h   \          \   scores           softmaxes
+        #               (+)------(ReLU)-----(*)--------(+)--------(softmax)----------
+        # b1            /  drelu        dh      dscores    dscores 
+        # -------------+
+        # db1    
+        
+        softmaxes = np.exp(lim_scores) / np.sum(np.exp(lim_scores), axis=1)[:,None]
+        # Gradient of softmax (derivative of softmax - see softmax.py)
+        dscores = softmaxes
+        dscores[np.arange(N), y] -= 1
+        dscores /= N
+        
+        grads['b2'] = np.sum(dscores, axis=0)  # shape (N,C) into (C,)
+        grads['W2'] = h.T.dot(dscores) + reg * W2
+        
+        dh = dscores.dot(W2.T)
+        drelu = (h > 0) * dh
+        
+        grads['b1'] = np.sum(drelu, axis=0)
+        grads['W1'] = X.T.dot(drelu) + reg * W1
+
+
+        ########################################################################
+        #                              END OF YOUR CODE                        #
+        ########################################################################
+
+        return loss, grads
+
+    def train(self, X, y, X_val, y_val,
+              learning_rate=1e-3, learning_rate_decay=0.95,
+              reg=1e-5, num_iters=100,
+              batch_size=200, verbose=False):
+        """
+        Train this neural network using stochastic gradient descent.
+
+        Inputs:
+        - X: A numpy array of shape (N, D) giving training data.
+        - y: A numpy array f shape (N,) giving training labels; y[i] = c means
+          that X[i] has label c, where 0 <= c < C.
+        - X_val: A numpy array of shape (N_val, D) giving validation data.
+        - y_val: A numpy array of shape (N_val,) giving validation labels.
+        - learning_rate: Scalar giving learning rate for optimization.
+        - learning_rate_decay: Scalar giving factor used to decay the learning
+          rate after each epoch.
+        - reg: Scalar giving regularization strength.
+        - num_iters: Number of steps to take when optimizing.
+        - batch_size: Number of training examples to use per step.
+        - verbose: boolean; if true print progress during optimization.
+        """
+        # pylint: disable=too-many-arguments, too-many-locals
+        num_train = X.shape[0]
+        iterations_per_epoch = max(num_train // batch_size, 1)
+
+        # Use SGD to optimize the parameters in self.model
+        loss_history = []
+        train_acc_history = []
+        val_acc_history = []
+
+        for it in range(num_iters):
+            X_batch = None
+            y_batch = None
+
+            ####################################################################
+            # TODO: Create a random minibatch of training data and labels,     #
+            # storing hem in X_batch and y_batch respectively.                 #
+            ####################################################################
+
+            idxs = np.random.choice(num_train, batch_size)
+            X_batch = X[idxs,:]
+            y_batch = y[idxs]
+            ####################################################################
+            #                             END OF YOUR CODE                     #
+            ####################################################################
+
+            # Compute loss and gradients using the current minibatch
+            loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
+            loss_history.append(loss)
+
+            ####################################################################
+            # TODO: Use the gradients in the grads dictionary to update the    #
+            # parameters of the network (stored in the dictionary self.params) #
+            # using stochastic gradient descent. You'll need to use the        #
+            # gradients stored in the grads dictionary defined above.          #
+            ####################################################################
+
+            self.params['W1'] -= learning_rate * grads['W1']
+            self.params['b1'] -= learning_rate * grads['b1']
+            self.params['W2'] -= learning_rate * grads['W2']
+            self.params['b2'] -= learning_rate * grads['b2']
+
+
+            ####################################################################
+            #                             END OF YOUR CODE                     #
+            ####################################################################
+
+            if verbose and it % 100 == 0:
+                print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+
+            # Every epoch, check train and val accuracy and decay learning rate.
+            if it % iterations_per_epoch == 0:
+                # Check accuracy
+                train_acc = (self.predict(X_batch) == y_batch).mean()
+                val_acc = (self.predict(X_val) == y_val).mean()
+                train_acc_history.append(train_acc)
+                val_acc_history.append(val_acc)
+
+                # Decay learning rate
+                learning_rate *= learning_rate_decay
+
+        return {
+            'loss_history': loss_history,
+            'train_acc_history': train_acc_history,
+            'val_acc_history': val_acc_history,
+        }
+
+    def predict(self, X):
+        """
+        Use the trained weights of this two-layer network to predict labels for
+        data points. For each data point we predict scores for each of the C
+        classes, and assign each data point to the class with the highest score.
+
+        Inputs:
+        - X: A numpy array of shape (N, D) giving N D-dimensional data points to
+          classify.
+
+        Returns:
+        - y_pred: A numpy array of shape (N,) giving predicted labels for each
+          of the elements of X. For all i, y_pred[i] = c means that X[i] is
+          predicted to have class c, where 0 <= c < C.
+        """
+        y_pred = None
+
+        ########################################################################
+        # TODO: Implement this function; it should be VERY simple!             #
+        ########################################################################
+
+        relu = lambda x: np.maximum(0, x) # ReLU activation function
+        h = relu(X.dot(self.params['W1']) + self.params['b1'])
+        scores = h.dot(self.params['W2']) + self.params['b2']
+        y_pred = np.argmax(scores, axis=1)
+
+
+        ########################################################################
+        #                              END OF YOUR CODE                        #
+        ########################################################################
+
+        return y_pred
+
+
+def neuralnetwork_hyperparameter_tuning(X_train, y_train, X_val, y_val):
+    best_net = None # store the best model into this 
+
+    ############################################################################
+    # TODO: Tune hyperparameters using the validation set. Store your best     #
+    # trained model in best_net.                                               #
+    #                                                                          #
+    # To help debug your network, it may help to use visualizations similar to #
+    # the  ones we used above in the Jupyther Notebook; these visualizations   #
+    # will have significant qualitative differences from the ones we saw for   #
+    # the poorly tuned network.                                                #
+    #                                                                          #
+    # Tweaking hyperparameters by hand can be fun, but you might find it useful#
+    # to  write code to sweep through possible combinations of hyperparameters #
+    # automatically like we did on the previous exercises.                     #
+    ############################################################################
+    # ABAHNASY: Define input size and number of classes
+    input_size = 32 * 32 * 3
+    num_classes = 10
+    # ABAHNASY: tracking values
+    best_val = -1
+    best_stats = []
+    # ABAHNASY: Set a seed for results reproduction
+    np.random.seed(0)
+    for i in range(20):
+        ## Strategy to find the best hyperparameters over 52% on the validation set
+        # Use generate_random function given some interval with 500 iterations
+        #lr, reg, hidden_size = generate_random_hyperparams(-6, -3, -5, 5, 20, 3000)
+        #lr, reg, hidden_size = generate_random_hyperparams(-4, -2, -2, 2, 20, 3000)
+        #lr, reg, hidden_size = generate_random_hyperparams(-4, -3, -1, 0, 10, 300)
+        
+        # According to the previous results, reduce the exploration by selecting set of fixed ranges
+        # use this ranges in the random search function to explore random combinations
+        #lr, reg, hidden_size = random_search_hyperparams([0.001, 0.002, 0.003], [0.1, 0.2, 0.3, 0.4, 0.5], [10, 50, 100, 150, 200])
+        #lr, reg, hidden_size = random_search_hyperparams([0.001], [0.1, 0.15, 0.2, 0.3], [10, 20, 30, 40 ,50, 80, 100, 150 , 200])    
+        
+        # Given a set of potential values, increase the number of iterations
+        lr, reg, hidden_size = random_search_hyperparams([0.001], [0.05, 0.1, 0.15], [50, 80, 100, 120, 150, 180, 200])
+        
+        # Create a two-layer network
+        net = TwoLayerNet(input_size, hidden_size, num_classes)
+        
+        # Train the network
+        stats = net.train(X_train, y_train, X_val, y_val,
+                    num_iters=2000, batch_size=200,
+                    learning_rate=lr, learning_rate_decay=0.89,
+                    reg=reg, verbose=False)
+
+        # Predict on the training set
+        train_accuracy = (net.predict(X_train) == y_train).mean()
+        
+        # Predict on the validation set
+        val_accuracy = (net.predict(X_val) == y_val).mean()
+        
+        # Save best values
+        if val_accuracy > best_val:
+            best_val = val_accuracy
+            best_net = net
+            best_stats = stats
+        
+        # Print results
+        print('lr %e reg %e hid %d  train accuracy: %f val accuracy: %f' % (
+                lr, reg, hidden_size, train_accuracy, val_accuracy))
+    print('best validation accuracy achieved: %f' % best_val)
+    
+
+    ############################################################################
+    #                               END OF YOUR CODE                           #
+    ############################################################################
+    return best_net
+
+# ABAHNASY: generate random hyperparameters given ranges for each of them
+def generate_random_hyperparams(lr_min, lr_max, reg_min, reg_max, h_min, h_max):
+    lr = 10**np.random.uniform(lr_min,lr_max)
+    reg = 10**np.random.uniform(reg_min,reg_max)
+    hidden = np.random.randint(h_min, h_max)
+    return lr, reg, hidden
+
+# ABAHNASY: get random hyperparameters given arrays of potential values
+def random_search_hyperparams(lr_values, reg_values, h_values):
+    lr = lr_values[np.random.randint(0,len(lr_values))]
+    reg = reg_values[np.random.randint(0,len(reg_values))]
+    hidden = h_values[np.random.randint(0,len(h_values))]
+    return lr, reg, hidden
